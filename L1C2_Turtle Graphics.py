adf83968d7700e6e4eb50f62c3632b5290729500
AK9dev <ak9dev@gmail.com>
2017-09-10 16:43:49 -0700

    Add files via upload

---
+++ L1C2_Turtle Graphics.py
@@ -0,0 +1,43 @@
+import turtle
+
+turtle.shape("square")
+turtle.pensize(1)
+turtle.color("red")
+
+
+
+
+for i in range (6):
+    turtle.fd(200)
+    turtle.lt(90)
+    turtle.fd(200)
+    
+   
+    turtle.lt(90)
+    turtle.fd(200)
+
+    turtle.lt(90)
+    turtle.fd(200)
+    turtle.color("green")
+    turtle.fd(200)
+    turtle.lt(90)
+
+    turtle.fd(200)
+    turtle.lt(90)
+
+    turtle.fd(200)
+    turtle.lt(90)
+    turtle.fd(200)
+    turtle.color("blue")
+    turtle.fd(200)
+    turtle.lt(90)
+
+    turtle.fd(200)
+    turtle.lt(90)
+
+    turtle.fd(200)
+    turtle.lt(90)
+    
+    turtle.fd(200)
+    turtle.fd(200)
+    turtle.exitonclick()
